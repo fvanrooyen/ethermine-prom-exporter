1662ca07d689fa5e4418cd35cd83f62c7077ae7d
Frans van Rooyen <vanrooye@adobe.com>
2017-08-10 16:38:19 -0600

    Added Server Functionality

--- ethermine-export.py
+++ ethermine-export.py
@@ -13,7 +13,15 @@ version = 0.50
 # Parse commandline arguments
 parser = argparse.ArgumentParser(description="EtherminePrometheus exporter v" + str(version))
 parser.add_argument("-m", "--miner", metavar="<mid>", required=True, help="Miner Address")
+parser.add_argument("-p", "--port", metavar="<port>", required=False, help="Port for listenin", default=8701, type=int)
+parser.add_argument("-f", "--frequency", metavar="<seconds>", required=False, help="Interval in seconds between checking measures", default=1, type=int)
 args = parser.parse_args()
+
+#Get the Arguments
+mineraddress = args.miner
+listenport = args.port
+sleep_time = args.frequency
+
 
 REQUEST_ADDRESS = Gauge('Ethermine_Address','Etermine Address',['MinerID'])
 REQUEST_REPORTED_HASH = Gauge('Ethermine_Reported_Hash_Rate', 'Ethermine Reported Hash Rate')
@@ -22,17 +30,24 @@ REQUEST_PROGRESS_PAYOUT = Gauge('Ethermine_Progress_Payout', 'Ethermine Payout P
 REQUEST_ACTIVE_WORKERS= Gauge('Ethermine_Active_Workers', 'Ethermine Active Workers')
 
 if __name__ == "__main__":
-    r = requests.get('https://ethermine.org/api/miner_new/' + args.miner)
-    data = json.loads(r.text)
-    REQUEST_ADDRESS.labels(data["address"])
-    print(data["address"])
-    hashrate = data["reportedHashRate"]
-    hashrate = hashrate.split()
-    REQUEST_REPORTED_HASH.set(hashrate[0])
-    print(hashrate[0])
-    REQUEST_UNPAID_BALANCE.set(float(data['unpaid']) / 1000000000000000000)
-    print(float(data['unpaid']) / 1000000000000000000) 
-    REQUEST_PROGRESS_PAYOUT.set((float(data['unpaid']) / 10000000000000000)/.5)
-    print((float(data['unpaid']) / 10000000000000000)/.5)
-    REQUEST_ACTIVE_WORKERS.set(data["minerStats"]["activeWorkers"])
-    print(data["minerStats"]["activeWorkers"])
+    # Start up the server to expose the metrics.
+    start_http_server(listenport)
+
+    while True:
+        r = requests.get('https://ethermine.org/api/miner_new/' + mineraddress)
+        data = json.loads(r.text)
+        REQUEST_ADDRESS.labels(data["address"])
+        print(data["address"])
+        hashrate = data["reportedHashRate"]
+        hashrate = hashrate.split()
+        REQUEST_REPORTED_HASH.set(hashrate[0])
+        print(hashrate[0])
+        REQUEST_UNPAID_BALANCE.set(float(data['unpaid']) / 1000000000000000000)
+        print(float(data['unpaid']) / 1000000000000000000) 
+        REQUEST_PROGRESS_PAYOUT.set((float(data['unpaid']) / 10000000000000000)/.5)
+        print((float(data['unpaid']) / 10000000000000000)/.5)
+        REQUEST_ACTIVE_WORKERS.set(data["minerStats"]["activeWorkers"])
+        print(data["minerStats"]["activeWorkers"])
+
+        #Sleep before run again
+        time.sleep(sleep_time)
