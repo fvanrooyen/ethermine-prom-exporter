ca705f73b602e899cbe024d90c9b6d7db420e21f
Frans van Rooyen <vanrooye@adobe.com>
2017-08-10 11:49:57 -0600

    Added Prom Parts

--- ethermine-export.py
+++ ethermine-export.py
@@ -11,17 +11,24 @@ import requests
 
 version = 0.50
 
-REQUEST_ADDRESS = Gauge('Ethermine Address','Etermine Address')
-REQUEST_REPORTED_HASH = Gauge('Ethermine Reported Hash Rate', 'Ethermine Reported Hash Rate')
-REQUEST_UNPAID_BALANCE = Gauge('Ethermine Unpaid Balance', 'Ethermine Unpaid Balance')
-REQUEST_MINER_STATS = Gauge('Ethermine Miner Stats', 'Ethermine Miner Stats')
-REQUEST_ACTIVE_WORKERS= Gauge('Ethermine Active Workers', 'Ethermine Active Workers')
+REQUEST_ADDRESS = Gauge('Ethermine_Address','Etermine Address')
+REQUEST_REPORTED_HASH = Gauge('Ethermine_Reported_Hash_Rate', 'Ethermine Reported Hash Rate')
+REQUEST_UNPAID_BALANCE = Gauge('Ethermine_Unpaid_Balance', 'Ethermine Unpaid Balance')
+REQUEST_PROGRESS_PAYOUT = Gauge('Ethermine_Progress_Payout', 'Ethermine Payout Progress')
+REQUEST_ACTIVE_WORKERS= Gauge('Ethermine_Active_Workers', 'Ethermine Active Workers')
 
 if __name__ == "__main__":
     r = requests.get('https://ethermine.org/api/miner_new/5526624c135c0390b5b5EE0aC5A2063Fe19F9FD5')
     data = json.loads(r.text)
+    #REQUEST_ADDRESS.set(data["address"])
     print(data["address"])
-    print(data["reportedHashRate"])
-    print(float(data['unpaid']) / 1000000000000000000)
+    hashrate = data["reportedHashRate"]
+    hashrate = hashrate.split()
+    REQUEST_REPORTED_HASH.set(hashrate[0])
+    print(hashrate[0])
+    REQUEST_UNPAID_BALANCE.set(float(data['unpaid']) / 1000000000000000000)
+    print(float(data['unpaid']) / 1000000000000000000) 
+    REQUEST_PROGRESS_PAYOUT.set((float(data['unpaid']) / 10000000000000000)/.5)
     print((float(data['unpaid']) / 10000000000000000)/.5)
+    REQUEST_ACTIVE_WORKERS.set(data["minerStats"]["activeWorkers"])
     print(data["minerStats"]["activeWorkers"])
